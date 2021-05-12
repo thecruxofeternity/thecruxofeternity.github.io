a393b0d138234c477b2e51a09310a2dbdba3ef36
TheCruxOfEternity <84053296+TheCruxOfEternity@users.noreply.github.com>
2021-05-11 21:23:54 -0400

    Initial Commit
    base static site. No backend or controllers.

---
+++ Scripts/jscript.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    $('.parallax').parallax();
+
+    $('.carousel').carousel();
+
+    setInterval(function () {
+          $('.carousel').carousel('next');
+    }, 9000);
+
+    $('.modal').modal();
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var dropdowns = document.querySelectorAll('.dropdown-trigger');
+        M.Dropdown.init(dropdowns);
+    });
+
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrainWidth: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false, // Displays dropdown below the button
+        alignment: 'left', // Displays dropdown with edge aligned to the left of button
+        stopPropagation: false // Stops event propagation
+    });
+  });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.carousel');
+var instances = M.Carousel.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+});
+
+var submitButton = document.getElementById("submit_form");
+var form = document.getElementById("email_form");
+form.addEventListener("submit", function (e) {
+    setTimeout(function () {
+        submitButton.value = "Sending...";
+        submitButton.disabled = true;
+    }, 1);
+});
+
+function openMailer() {
+    window.open("mailto:mattfraley@outlook.com?subject=TheCruxOfExternity_DevMail");
+}
+
+
+
+
